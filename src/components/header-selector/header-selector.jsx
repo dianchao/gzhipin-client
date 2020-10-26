c4dd82f84b5a6cef490150bc34ca95bdd903f394
zdc <947389057@qq.com>
2020-10-26 23:46:59 +0800

    谷粒直聘客户端程序

--- src/components/header-selector/header-selector.jsx
+++ src/components/header-selector/header-selector.jsx
@@ -2,8 +2,8 @@
 选择用户头像的UI组件
  */
 
-import React, {Component} from 'react'
-import {List, Grid} from 'antd-mobile'
+import React, { Component } from 'react'
+import { List, Grid } from 'antd-mobile'
 import PropTypes from 'prop-types'
 
 export default class HeaderSelector extends Component {
@@ -22,33 +22,33 @@ export default class HeaderSelector extends Component {
     this.headerList = []
     for (let i = 0; i < 20; i++) {
       this.headerList.push({
-        text: '头像'+(i+1),
-        icon: require(`../../assets/images/头像${i+1}.png`) // 不能使用import
+        text: '头像' + (i + 1),
+        icon: require(`../../assets/images/头像${i + 1}.png`) // 不能使用import
       })
     }
   }
 
-  handleClick = ({text, icon}) => {
+  handleClick = ({ text, icon }) => {
     // 更新当前组件状态
-    this.setState({icon})
+    this.setState({ icon })
     // 调用函数更新父组件状态
     this.props.setHeader(text)
   }
 
-  render () {
+  render() {
     // 头部界面
-    const {icon} = this.state
+    const { icon } = this.state
     const listHeader = !icon ? '请选择头像' : (
       <div>
-        已选择头像:<img src={icon}/>
+        已选择头像:<img src={icon} />
       </div>
     )
 
     return (
       <List renderHeader={() => listHeader}>
         <Grid data={this.headerList}
-              columnNum={5}
-              onClick={this.handleClick}/>
+          columnNum={5}
+          onClick={this.handleClick} />
       </List>
     )
   }
